032e7f9ec17bfd4a1156c4271ca1f0fa4c1f4e55
Brian Yarr <brian.yarr@bgch.co.uk>
2017-03-19 20:03:54 +0000

    Refactor type picking.

--- src/main/kotlin/com/brianyarr/zephros/cfnspec/CodeGen.kt
+++ src/main/kotlin/com/brianyarr/zephros/cfnspec/CodeGen.kt
@@ -2,39 +2,50 @@ package com.brianyarr.zephros.cfnspec
 
 object CodeGen {
 
-    fun Property.getKotlinType(): String {
-        if (Type == null && PrimitiveType == null) {
-            throw IllegalStateException()
-        }
-        if (Type != null && PrimitiveType != null) {
-            throw IllegalStateException()
-        }
+    fun Property.getKotlinType(itemTypePrefix: String): String {
 
-        if (ItemType != null && PrimitiveItemType != null) {
-            throw IllegalStateException()
-        }
-
-        var type = Type ?: "" + PrimitiveType?: ""
+        var type = chooseType(Type, PrimitiveType, itemTypePrefix)
         if (ItemType != null || PrimitiveItemType != null) {
-            type = type + "<" + (ItemType ?: "") + (PrimitiveItemType ?: "") + ">"
+            type = type + chooseType(ItemType, PrimitiveItemType, itemTypePrefix) + ">"
         }
+
         if (!Required) {
             type += "?"
         }
         return type
     }
 
+    fun chooseType(Type: String?, PrimitiveType: String?, prefix: String): String {
+        if (Type != null && PrimitiveType != null) {
+            throw IllegalArgumentException()
+        }
+        if (Type != null) {
+            if (Type.equals("List")) {
+                return Type + "<"
+            } else if (Type.equals("Map")) {
+                return Type + "<String, "
+            } else{
+                return prefix + Type
+            }
+        }
+        if (PrimitiveType != null){
+            return PrimitiveType
+        }
+        throw IllegalArgumentException()
+    }
+
     fun dataClass(name: String, pt: PropertyType): StringBuilder {
         val result = StringBuilder()
         val className = name.replace("::", "_").replace(".", "_")
 
+        val prefix = name.split('.')[0].replace("::", "_") + "_"
 
         val docs = DocFetcher.getDocs(pt)
         result.append("/**").append(docs).append("*/\n")
 
         result.append("data class ").append(className).append("(")
         result.append(pt.Properties.map {
-            "val " + it.key + ": " + it.value.getKotlinType()
+            "val " + it.key + ": " + it.value.getKotlinType(prefix)
         }.joinToString(separator = ", ")
         )
 
